694a6a377d64c9cf5cef261700443969bf6289d0
Selma <selma.waller@glasspaper.no>
2024-04-23 12:09:44 +0200

    more js docs

--- src/components/docs/JSDocs.vue
+++ src/components/docs/JSDocs.vue
@@ -1,5 +1,3 @@
-<script setup lang="ts"></script>
-
 <template>
   <!-- Control Flow Basics -->
   <div class="pb-10">
@@ -134,11 +132,11 @@
         </p>
         <p><span class="pr-6 text-slate-400">9</span></p>
         <p class="text-slate-50 indent">
-          <span class="pr-6 text-slate-400">10</span>if(!user) {
+          <span class="pr-4 text-slate-400">10</span>if(!user) {
           console.log('You must be logged in to continue') }
         </p>
         <p class="text-slate-50 indent">
-          <span class="pr-6 text-slate-400">11</span>
+          <span class="pr-4 text-slate-400">11</span>
           <span class="text-green-400 italic">true</span>
         </p>
       </div>
@@ -203,47 +201,47 @@
           break;
         </p>
         <p class="text-slate-50 indent">
-          <span class="pr-4 text-slate-400">10</span>
+          <span class="pr-2 text-slate-400">10</span>
           case 'C':
         </p>
         <p class="text-slate-50 indent">
-          <span class="pr-8 text-slate-400">11</span>
+          <span class="pr-6 text-slate-400">11</span>
           console.log('You got a C')
         </p>
         <p class="text-slate-50 indent">
-          <span class="pr-8 text-slate-400">12</span>
+          <span class="pr-6 text-slate-400">12</span>
           break;
         </p>
         <p class="text-slate-50 indent">
-          <span class="pr-4 text-slate-400">13</span>
+          <span class="pr-2 text-slate-400">13</span>
           case 'D':
         </p>
         <p class="text-slate-50 indent">
-          <span class="pr-8 text-slate-400">14</span>
+          <span class="pr-6 text-slate-400">14</span>
           console.log('You got a D')
         </p>
         <p class="text-slate-50 indent">
-          <span class="pr-8 text-slate-400">15</span>
+          <span class="pr-6 text-slate-400">15</span>
           break;
         </p>
         <p class="text-slate-50 indent">
-          <span class="pr-4 text-slate-400">16</span>
+          <span class="pr-2 text-slate-400">16</span>
           case 'F':
         </p>
         <p class="text-slate-50 indent">
-          <span class="pr-8 text-slate-400">17</span>
+          <span class="pr-6 text-slate-400">17</span>
           console.log('You got an F')
         </p>
         <p class="text-slate-50 indent">
-          <span class="pr-8 text-slate-400">18</span>
+          <span class="pr-6 text-slate-400">18</span>
           break;
         </p>
         <p class="text-slate-50 indent">
-          <span class="pr-4 text-slate-400">19</span>
+          <span class="pr-2 text-slate-400">19</span>
           default:
         </p>
         <p class="text-slate-50 indent">
-          <span class="pr-8 text-slate-400">20</span>
+          <span class="pr-6 text-slate-400">20</span>
           console.log('Not a valid grade')
         </p>
         <p class="text-slate-50 indent">
@@ -253,7 +251,7 @@
           <span class="pr-6 text-slate-400">22</span>
         </p>
         <p class="text-slate-50 indent">
-          <span class="pr-6 text-slate-400">23</span>
+          <span class="pr-4 text-slate-400">23</span>
           <span class="text-green-400 italic">You got a D</span>
         </p>
       </div>
@@ -352,16 +350,13 @@
           <span class="pr-6 text-slate-400">3</span>const area = calcArea(5);
           <span class="text-green-400 italic">78.5</span>
         </p>
-        <p class="text-slate-50 indent">
-          <span class="pr-6 text-slate-400">4</span>
-        </p>
       </div>
 
       <p class="text-slate-700 pb-2" id="arrow-functions">Arrow functions</p>
       <ul class="pb-5 text-slate-800">
         <li>
-          - Arrow functions are different than regular functions when refering
-          to 'this':....
+          - The value of "this" does not change from the value it was where the
+          arrow function is invoked
         </li>
       </ul>
       <div class="bg-gray-700 rounded p-4 mb-6 font-mono">
@@ -375,9 +370,6 @@
         <p class="text-slate-50 indent">
           <span class="pr-6 text-slate-400">3</span>const area = calcArea(5);
           <span class="text-green-400 italic">78.5</span>
-        </p>
-        <p class="text-slate-50 indent">
-          <span class="pr-6 text-slate-400">4</span>
         </p>
       </div>
 
@@ -419,6 +411,9 @@
       <p class="text-slate-700 pb-2" id="callback-functions-and-foreach">
         Callback functions & forEach
       </p>
+      <ul class="pb-5 text-slate-800">
+        <li>- Functions/methods passed in as arguments</li>
+      </ul>
       <div class="bg-gray-700 rounded p-4 mb-6 font-mono">
         <p class="text-slate-50 indent">
           <span class="pr-6 text-slate-400">1</span>const myFunc =
@@ -457,11 +452,312 @@
           <span class="pr-6 text-slate-400">10</span>
         </p>
         <p class="text-slate-50 indent">
-          <span class="pr-6 text-slate-400">11</span>people.forEach(logPerson)
+          <span class="pr-4 text-slate-400">11</span>people.forEach(logPerson)
         </p>
       </div>
-      <hr />
+
+      <p class="text-slate-700 pb-2" id="callback-functions-in-action">
+        Callback functions in action
+      </p>
+      <div class="bg-gray-700 rounded p-4 mb-6 font-mono">
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">1</span>const ul =
+          document.querySelector('.people')
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">2</span>
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">3</span>const people = ['Mario',
+          'Luigi', 'Peach']
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">4</span>
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">5</span>let html = ``
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">6</span>
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">7</span>people.forEach(person => {
+          html += `&lt;li style="color: purple"&gt;${person}&lt;/li&gt;`})
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">8</span>
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">9</span>console.log(html)
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">10</span>
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-4 text-slate-400">11</span>people.forEach(logPerson)
+        </p>
+      </div>
     </div>
+    <hr />
+  </div>
+
+  <!-- Object Literals -->
+  <div class="pb-10">
+    <div class="pb-6">
+      <h3 class="text-2xl mb-2 text-slate-800" id="object-literals">
+        Object Literals
+      </h3>
+      <p class="text-slate-700 pb-2" id="creating-an-object">
+        Creating an object
+      </p>
+      <div class="bg-gray-700 rounded p-4 mb-6 font-mono">
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">1</span>let user = { name: 'Mario',
+          age: 30 }
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">2</span>
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">3</span>user.age = 35
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">4</span>console.log(user['age'])
+          <span class="pr-6 text-green-400 italic">equal to user.age</span>
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">5</span>
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">6</span>const key = 'location'
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">7</span>console.log(user[key])
+          <span class="pr-6 text-green-400 italic"
+            >equal to user['location']</span
+          >
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">8</span>
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">9</span>console.log(typeof user)
+          <span class="pr-6 text-green-400 italic">object</span>
+        </p>
+      </div>
+
+      <p class="text-slate-700 pb-2" id="adding-methods">Adding methods</p>
+      <div class="bg-gray-700 rounded p-4 mb-6 font-mono">
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">1</span>let user = { name: 'Mario',
+          login: function(){ console.log('the user logged in')} }
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">2</span>
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">3</span>user.login()
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">4</span>
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">5</span>const name = 'Luigi'
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">6</span>name.toUpperCase()
+        </p>
+      </div>
+
+      <p class="text-slate-700 pb-2" id="this-keyword">'this' keyword</p>
+      <ul class="pb-5 text-slate-800">
+        <li>
+          - When using arrow functions, the 'this' keyword is out of context
+          because it refers to the state it was in at the place it was invoked
+          => outside the object.
+        </li>
+      </ul>
+      <div class="bg-gray-700 rounded p-4 mb-6 font-mono">
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">1</span>let user = { name: 'Mario',
+          blogs: ['pipes', 'mushrooms'], logBlogs() { this.blogs.forEach(blog =>
+          { console.log(blog) }) } }
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">2</span>
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">3</span>
+          <span class="pr-6 text-green-400 italic"
+            >This user has written the following blogs:</span
+          >
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-8 text-slate-400"></span>
+          <span class="pr-6 text-green-400 italic">pipes</span>
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-8 text-slate-400"></span>
+          <span class="pr-6 text-green-400 italic">mushrooms</span>
+        </p>
+      </div>
+
+      <p class="text-slate-700 pb-2" id="objects-in-arrays">
+        Objects in arrays
+      </p>
+      <div class="bg-gray-700 rounded p-4 mb-6 font-mono">
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">1</span>const blogs = [ { title:
+          'pipes', likes: 0 }, { title: 'mushrooms', likes: 200 } ]
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">2</span>let user = { name: 'Mario',
+          blogs = [ { title: 'pipes', likes: 0 }, { title: 'mushrooms', likes:
+          200 } ], logBlogs() { this.blogs.forEach(blog => {
+          console.log(blog.title, blog.likes) }) } }
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">3</span>
+          <span class="pr-6 text-green-400 italic"
+            >This user has written the following blogs:</span
+          >
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-8 text-slate-400"></span>
+          <span class="pr-6 text-green-400 italic">pipes 0</span>
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-8 text-slate-400"></span>
+          <span class="pr-6 text-green-400 italic">mushrooms 200</span>
+        </p>
+      </div>
+
+      <p class="text-slate-700 pb-2" id="math-object">Math object</p>
+      <div class="bg-gray-700 rounded p-4 mb-6 font-mono">
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">1</span>const area = 7.7;
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">2</span>
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">3</span>Math.round(area)
+          <span class="pr-6 text-green-400 italic">8 - rounds up or down</span>
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">4</span>Math.floor(area)
+          <span class="pr-6 text-green-400 italic"
+            >7 - rounds down whatever the decimal</span
+          >
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">5</span>Math.ceil(area)
+          <span class="pr-6 text-green-400 italic"
+            >8 - rounds up whatever the decimal</span
+          >
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">6</span>Math.trunc(area)
+          <span class="pr-6 text-green-400 italic"
+            >7 - removes the decimal</span
+          >
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">7</span>Math.random()
+          <span class="pr-6 text-green-400 italic"
+            >random number between 0 and 1</span
+          >
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">8</span>Math.round(random())
+          <span class="pr-6 text-green-400 italic"
+            >random number between 0 and 1</span
+          >
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">9</span>Math.round(random() * 100)
+          <span class="pr-6 text-green-400 italic"
+            >random number between 1 and 100</span
+          >
+        </p>
+      </div>
+
+      <p class="text-slate-700 pb-2" id="primitive-values">Primitive values</p>
+      <div class="bg-gray-700 rounded p-4 mb-6 font-mono">
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">1</span>let scoreOne = 50;
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">2</span>let scoreTwo = scoreOne;
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">3</span>
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">4</span>`scoreOne: ${scoreOne},
+          scoreTwo: ${scoreTwo}`;
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">5</span>
+          <span class="pr-6 text-green-400 italic"
+            >scoreOne: 50, scoreTwo: 50</span
+          >
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">6</span>
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">7</span>scoreOne = 100;
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">9</span>
+          <span class="pr-6 text-green-400 italic"
+            >scoreOne: 100, scoreTwo: 50</span
+          >
+        </p>
+      </div>
+
+      <p class="text-slate-700 pb-2" id="Reference-values">Reference values</p>
+      <div class="bg-gray-700 rounded p-4 mb-6 font-mono">
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">1</span>const userOne = { name:
+          'Mario', age: 30 };
+          <span class="pr-6 text-green-400 italic"
+            >(object => reference type)</span
+          >
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">2</span>const userTwo = userOne;
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">3</span>
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">4</span>console.log(usreOne,
+          userTwo)
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">5</span
+          ><span class="pr-6 text-green-400 italic"
+            >{name: 'Mario', age: 30} {name: 'Mario', age: 30}</span
+          >
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">5</span>userOne.age = 40;
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">4</span>console.log(usreOne,
+          userTwo)
+        </p>
+        <p class="text-slate-50 indent">
+          <span class="pr-6 text-slate-400">5</span
+          ><span class="pr-6 text-green-400 italic"
+            >{name: 'Mario', age: 40} {name: 'Mario', age: 40}</span
+          >
+        </p>
+      </div>
+    </div>
+    <hr />
   </div>
 </template>
 
